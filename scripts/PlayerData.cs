857e172a5f8be4a2802f498645966d0c0244af4d
Joshua Higgins <joshuafhiggins@gmail.com>
2025-12-02 19:37:21 -0500

    feat: connection backbone

---
+++ scripts/PlayerData.cs
@@ -0,0 +1,13 @@
+public class PlayerData
+{
+    public string username { get; private set; }
+    public bool isReady { get; private set; }
+    public bool isPlaying { get; private set; }
+
+    public PlayerData(string username, bool isReady, bool isPlaying)
+    {
+        this.username = username;
+        this.isReady = isReady;
+        this.isPlaying = isPlaying;
+    }
+}
